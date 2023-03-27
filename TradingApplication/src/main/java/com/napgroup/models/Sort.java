9906d35d58071ffc3435b290da201926820ed15d
Nabeeha <nabeehak@icloud.com>
2023-03-27 21:35:34 +0100

    Implemented Sort model

---
+++ TradingApplication/src/main/java/com/napgroup/models/Sort.java
@@ -0,0 +1,33 @@
+package com.napgroup.models;
+
+import java.util.Map;
+
+public class Sort {
+	
+	// REGION -> orderBook for that region
+	private Map<Region, OrderBook> orderBooks;
+	private Company company;
+	
+	public Sort(Map<Region, OrderBook> orderBooks, Company company) {
+		super();
+		this.orderBooks = orderBooks;
+		this.company = company;
+	}
+
+	public Map<Region, OrderBook> getOrderBooks() {
+		return orderBooks;
+	}
+
+	public void setOrderBooks(Map<Region, OrderBook> orderBooks) {
+		this.orderBooks = orderBooks;
+	}
+
+	public Company getCompany() {
+		return company;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+
+}

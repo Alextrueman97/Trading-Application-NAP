7f513255d7e3056960e44c4da8d013113a5c969d
Nabeeha <nabeehak@icloud.com>
2023-03-30 09:12:51 +0100

    Implemented service methods ofr AskOrders

--- TradingApplication/src/main/java/com/napgroup/services/AskOrdersService.java
+++ TradingApplication/src/main/java/com/napgroup/services/AskOrdersService.java
@@ -4,16 +4,27 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.napgroup.models.AskOrders;
 import com.napgroup.models.OrderStatus;
 import com.napgroup.models.OrderType;
+import com.napgroup.models.Region;
 import com.napgroup.models.SaleType;
 import com.napgroup.models.Stocks;
 
+import jakarta.transaction.Transactional;
+
 public interface AskOrdersService {
 	
-	
-	public List<AskOrders> findUserAskOrders(int accountId);
-	public Optional<AskOrders> findOrder(int orderId);
+	public List<AskOrders> findUserAskOrdersByAccountId( int accountId);
+	public List<AskOrders> findIncompleteOrdersByCompanyAndRegion(int companyId, Region region);
+	public List<AskOrders> findCompleteOrdersByAccountId(int accountId);
+	public AskOrders addAskOrder(AskOrders askOrder);
+	public AskOrders updateOrderStatus(int orderId, OrderStatus orderStatus);
+	public AskOrders updateStockAmount(int orderId, int stockAmount);
+	public AskOrders updateStockPrice(int orderId, double salePrice);
 
 }
--- TradingApplication/src/main/java/com/napgroup/services/AskOrdersServiceImpl.java
+++ TradingApplication/src/main/java/com/napgroup/services/AskOrdersServiceImpl.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.napgroup.models.AskOrders;
+import com.napgroup.models.OrderStatus;
+import com.napgroup.models.Region;
 import com.napgroup.repositories.AskOrdersRepository;
 
 @Service
@@ -16,13 +18,38 @@ public class AskOrdersServiceImpl implements AskOrdersService {
 	private AskOrdersRepository askOrdersRepository;
 
 	@Override
-	public List<AskOrders> findUserAskOrders(int accountId) {
-		return askOrdersRepository.findUserAskOrders(accountId);
+	public List<AskOrders> findUserAskOrdersByAccountId(int accountId) {
+		return askOrdersRepository.findUserAskOrdersByAccountId(accountId);
+	}
+
+	@Override
+	public List<AskOrders> findIncompleteOrdersByCompanyAndRegion(int companyId, Region region) {
+		return askOrdersRepository.findIncompleteOrdersByCompanyAndRegion(companyId, region);
+	}
+
+	@Override
+	public List<AskOrders> findCompleteOrdersByAccountId(int accountId) {
+		return askOrdersRepository.findCompleteOrdersByAccountId(accountId);
 	}
 	
 	@Override
-	public Optional<AskOrders> findOrder(int orderId) {
-		return askOrdersRepository.findById(orderId);
+	public AskOrders addAskOrder(AskOrders askOrder) {
+		return askOrdersRepository.save(askOrder);
+	}
+
+	@Override
+	public AskOrders updateOrderStatus(int orderId, OrderStatus orderStatus) {
+		return askOrdersRepository.updateOrderStatus(orderId, orderStatus);
+	}
+
+	@Override
+	public AskOrders updateStockAmount(int orderId, int stockAmount) {
+		return askOrdersRepository.updateStockAmount(orderId, stockAmount);
+	}
+
+	@Override
+	public AskOrders updateStockPrice(int orderId, double salePrice) {
+		return askOrdersRepository.updateStockPrice(orderId, salePrice);
 	}
 
 }

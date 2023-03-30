69ee05671c96679a4c7f0544a91aac9771934308
Nabeeha <nabeehak@icloud.com>
2023-03-30 09:12:13 +0100

    Implemented queries for AskOrders

--- TradingApplication/src/main/java/com/napgroup/repositories/AskOrdersRepository.java
+++ TradingApplication/src/main/java/com/napgroup/repositories/AskOrdersRepository.java
@@ -3,14 +3,42 @@ package com.napgroup.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.napgroup.models.AskOrders;
+import com.napgroup.models.OrderStatus;
+import com.napgroup.models.Region;
+
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface AskOrdersRepository extends JpaRepository<AskOrders, Integer> {
 
 	@Query(value = "select new AskOrders(a.orderId, a.accountId, a.stockId, a.salePrice, a.stockAmount, a.orderStatus, a.orderType, a.saleType, a.saleDate) from AskOrders a where a.accountId.accountId = :accountId")
-	public List<AskOrders> findUserAskOrders(int accountId);
+	public List<AskOrders> findUserAskOrdersByAccountId(@Param("accountId") int accountId);
+	
+	@Query(value = "select new AskOrders(a.orderId, a.accountId, a.stockId, a.salePrice, a.stockAmount, a.orderStatus, a.orderType, a.saleType, a.saleDate) from AskOrders a where a.stockId.companyId.companyId = :companyId and a.stockId.region = :region and (a.orderStatus = 'PENDING' or a.orderStatus = 'PARTIAL')")
+	public List<AskOrders> findIncompleteOrdersByCompanyAndRegion(@Param("companyId") int companyId, @Param("region") Region region);
+	
+	@Query(value = "select new AskOrders(a.orderId, a.accountId, a.stockId, a.salePrice, a.stockAmount, a.orderStatus, a.orderType, a.saleType, a.saleDate) from AskOrders a where a.accountId.accountId = :accountId and a.orderStatus = 'COMPLETE'")
+	public List<AskOrders> findCompleteOrdersByAccountId(@Param("accountId") int accountId);
+
+	@Query(value = "update AskOrders a set a.orderStatus = :orderStatus where a.orderId = :orderId")
+	@Transactional
+	@Modifying
+	public AskOrders updateOrderStatus(@Param("orderId") int orderId, @Param("orderStatus") OrderStatus orderStatus);
+
+	@Query(value = "update AskOrders a set a.stockAmount = :stockAmount where a.orderId = :orderId")
+	@Transactional
+	@Modifying
+	public AskOrders updateStockAmount(@Param("orderId") int orderId, @Param("stockAmount") int stockAmount);
+	
+	@Query(value = "update AskOrders a set a.salePrice = :salePrice where a.orderId = :orderId")
+	@Transactional
+	@Modifying
+	public AskOrders updateStockPrice(@Param("orderId") int orderId, @Param("salePrice") double salePrice);
+
 }
--- TradingApplication/src/main/java/com/napgroup/repositories/StocksRepository.java
+++ TradingApplication/src/main/java/com/napgroup/repositories/StocksRepository.java
@@ -1,11 +1,21 @@
 package com.napgroup.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.napgroup.models.Stocks;
 
+import jakarta.transaction.Transactional;
+
 @Repository
 public interface StocksRepository extends JpaRepository<Stocks, Integer> {
 
+	@Query(value = "update Stocks s set s.stockAmount = :stockAmount where s.stockId = :stockId")
+	@Transactional
+	@Modifying
+	public int updateStockAmountById(@Param("stockId") int stockId, @Param("stockAmount") int stockAmount);
+	
 }

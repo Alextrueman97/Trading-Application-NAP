a2666aadcb4a25ab4b09d6139875a9f21c2d0515
Nabeeha <nabeehak@icloud.com>
2023-03-31 13:03:24 +0100

    Fixed issue with comparator

--- TradingApplication/src/main/java/com/napgroup/models/OrderTableSuperComparator.java
+++ TradingApplication/src/main/java/com/napgroup/models/OrderTableSuperComparator.java
@@ -13,10 +13,9 @@ public class OrderTableSuperComparator implements Comparator<OrderTableSuper> {
 		LocalDate o2Date = o2.getSaleDate().toLocalDate();
 		if(o1Date.isEqual(o2Date)) {
 			LocalTime o1Time = o1.getSaleDate().toLocalTime();
-			LocalTime o2Time = o1.getSaleDate().toLocalTime();
+			LocalTime o2Time = o2.getSaleDate().toLocalTime();
 			return o1Time.compareTo(o2Time);
 		}
-		System.out.println(o1Date + " compared to " + o2Date + ": " + o1Date.compareTo(o2Date));
 		return o1Date.compareTo(o2Date);
 	}
 

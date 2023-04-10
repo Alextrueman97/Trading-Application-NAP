b462e27ec54d6ced47dced2e51f823ab3b46887c
Nabeeha <nabeehak@icloud.com>
2023-04-10 18:57:28 +0100

    Removed Eureka dependency for testing purposes

--- TradingApplication/src/main/java/com/napgroup/TradingApplication.java
+++ TradingApplication/src/main/java/com/napgroup/TradingApplication.java
@@ -2,12 +2,12 @@ package com.napgroup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+// @EnableDiscoveryClient
 public class TradingApplication {
 
 	public static void main(String[] args) {
